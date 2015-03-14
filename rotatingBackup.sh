c78003f091087282331d4c2d7e93a7a2d3b4c3d3
Barnaby Colby <barneycolby@gmail.com>
2015-03-14 18:13:10 +0000

    Output is now piped to a log file as well as the screen.

--- rotatingBackup.sh
+++ rotatingBackup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Store the script start time so we can write it to the log later
+scriptStartTime="$(date '+%A %d %B %Y %X')"
+
 # Check that we've been passed a config file and that it's exists
 if [ $# -ne 1 ] || [ ! -f $1 ]
 then
@@ -12,6 +15,23 @@ source ~/backup/defaultConfig
 
 # Read in the variables from the config file that was passed in as an argument
 source $1
+
+# Redirect all program output to the logfile
+echo ${filePipePath}
+echo -n "Opening FIFO pipe....."
+mkfifo ${filePipePath}
+echo "Done."
+echo -n "Piping output to logfile as well as stdout....."
+tee ${logFile} < ${filePipePath} &
+teepid=$!
+exec > ${filePipePath} 2>&1
+echo "Done."
+
+# Clear the log
+> ${logFile}
+
+# Write the start time
+echo "Script started at: $scriptStartTime"
 
 # The first thing to do is to discard the last snapshot
 # We also remove any other snapshots that might exist as a result of the number of snapshots to create being lowered
@@ -50,3 +70,8 @@ then
 	# remove-destination removes the hard link between files if it's changed to prevent the overwrite changed the data in the previous backups
 	cp -al --remove-destination ${continuousDirectoryPath} ${snapshotDirectoryPath}/1
 fi
+
+# Clean up
+exec 1>&- 2>&-
+wait ${teepid}
+rm ${filePipePath}
