c25efabcbbc8207af82ef60290d858c2dc6bbc6a
Barnaby Colby <barneycolby@gmail.com>
2015-03-14 14:43:09 +0000

    Added daily backup script that backs up from the continuous directory and rotates the daily backups. Currently this stores 4 backups.

---
+++ dailyBackup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Read in the variables from the config file
+source /home/backup/backup/config
+
+# The first thing to do is to discard the last backup
+rm -rf ${dailiesDirectoryPath}/3
+
+# Now we can rotate the other daily snapshots
+mv ${dailiesDirectoryPath}/2 ${dailiesDirectoryPath}/3
+mv ${dailiesDirectoryPath}/1 ${dailiesDirectoryPath}/2
+mv ${dailiesDirectoryPath}/0 ${dailiesDirectoryPath}/1
+
+# Finally, we need to create the new daily snapshot
+# a flag preserves timestamps, owners etc. (Archive)
+# l flag copies files using hard links so that they use the same underlying file data
+# remove-destination removes the hard link between files if it's changed to prevent the overwrite changed the data in the previous backups
+cp -al --remove-destination ${continuousDirectoryPath} ${dailiesDirectoryPath}/0
