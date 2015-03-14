8e3259ba75ffa95e2b21dfa0a6d7f579a4cf471d
Barnaby Colby <barneycolby@gmail.com>
2015-03-14 14:45:54 +0000

    Changed daily backup file numbers to start at 1 instead of 0.

--- dailyBackup.sh
+++ dailyBackup.sh
@@ -4,15 +4,15 @@
 source /home/backup/backup/config
 
 # The first thing to do is to discard the last backup
-rm -rf ${dailiesDirectoryPath}/3
+rm -rf ${dailiesDirectoryPath}/4
 
 # Now we can rotate the other daily snapshots
+mv ${dailiesDirectoryPath}/3 ${dailiesDirectoryPath}/4
 mv ${dailiesDirectoryPath}/2 ${dailiesDirectoryPath}/3
 mv ${dailiesDirectoryPath}/1 ${dailiesDirectoryPath}/2
-mv ${dailiesDirectoryPath}/0 ${dailiesDirectoryPath}/1
 
 # Finally, we need to create the new daily snapshot
 # a flag preserves timestamps, owners etc. (Archive)
 # l flag copies files using hard links so that they use the same underlying file data
 # remove-destination removes the hard link between files if it's changed to prevent the overwrite changed the data in the previous backups
-cp -al --remove-destination ${continuousDirectoryPath} ${dailiesDirectoryPath}/0
+cp -al --remove-destination ${continuousDirectoryPath} ${dailiesDirectoryPath}/1
