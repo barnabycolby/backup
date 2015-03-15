aca5a1e26e98771092e4104efca6177d6c3c55e8
Barnaby Colby <barneycolby@gmail.com>
2015-03-15 17:58:46 +0000

    Changed rsyncPull script to actually perform an rsync pull from a hardcoded samba share to a hardcoded directory within the continuous backup directory.

--- scripts/rsyncPull.sh
+++ scripts/rsyncPull.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "rsync pull script"
+# Read in the variables from the config file
+source ~/backup/config/defaultConfig
+
+# The shareToPullFrom must end with a slash, otherwise rsync will create an extra directory under the destination directory
+shareToPullFrom='/mnt/backupShares/barney/'
+
+destination="${continuousDirectoryPath}/barney"
+
+if [ -e ${shareToPullFrom} ] && [ -e ${destination} ]
+then
+	echo "Pulling from the share into the continuous backup directory."
+	rsync -a --delete ${shareToPullFrom} ${destination}
+else
+	echo "Either the share to pull from or the destination directory did not exist."
+fi
