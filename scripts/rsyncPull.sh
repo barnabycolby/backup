e616f1851304d0e735134317de19bb150649ce8c
Barnaby Colby <barneycolby@gmail.com>
2015-03-15 18:07:00 +0000

    Generalised the rsyncpull script so that the name of the share is passed as an argument and the paths are specified in the config file.

--- scripts/rsyncPull.sh
+++ scripts/rsyncPull.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "You must only provide one argument, the name of the share."
+	exit 1
+fi
+
+# We take the first argument as the name of the share to backup
+shareToBackup="$1"
+
 # Read in the variables from the config file
 source ~/backup/config/defaultConfig
 
 # The shareToPullFrom must end with a slash, otherwise rsync will create an extra directory under the destination directory
-shareToPullFrom='/mnt/backupShares/barney/'
+shareToPullFrom="${backupShares}/${shareToBackup}/"
 
-destination="${continuousDirectoryPath}/barney"
+# The destination directory to pull from
+destination="${continuousDirectoryPath}/${shareToBackup}"
 
 if [ -e ${shareToPullFrom} ] && [ -e ${destination} ]
 then
