9e112923f432e69650e271de8df7f72453c6ef5f
Barnaby Colby <barneycolby@gmail.com>
2015-03-16 19:58:13 +0000

    Refactored BackupClient into a fully functioning object instead of having everything in main.

--- client/src/BackupClient.java
+++ client/src/BackupClient.java
@@ -5,69 +5,100 @@ import java.io.*;
  * The client that interacts with a backup server. Currently it allows the user to send a message to the server and print's the server's response.
  */
 public class BackupClient {
+
 	public static void main(String[] args) {
+		BackupClient backupClient = null;
 		try {
-			// Load the config file
-			ConfigReader config = null;
-			try {
-				config = new ConfigReader("./clientConfig");
-			}
-			catch (IOException e) {
-				throw new Exception("Could not open config file: " + e.getMessage());
-			}
-
-			String serverIP = config.getSetting("serverIP");
-
-			// Get the port number to connect to the server on
-			String portNumberAsString = config.getSetting("port");
-			int portNumber;
-			try {
-				portNumber = Integer.parseInt(portNumberAsString);
-			}
-			catch (NumberFormatException e) {
-				throw new Exception("The port number in the config file was not a number.");
-			}
-
-			// Create a socket and the objects for communication
-			Socket socket = new Socket(serverIP, portNumber);
-			PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			// Send the client's identity to the server
-			String identity = config.getSetting("identity");
-			socketWriter.println(identity);
-
-			// Check that the server recognised our identity
-			String serverResponse = socketReader.readLine();
-			if (!serverResponse.equals("Recognised")) {
-				throw new Exception("The server didn't recognise our identity: " + serverResponse);
-			}
-
-			// Send user's message to the server and print the response
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-			String userInput;
-
-			System.out.println("If you would like to exit at any time, simply type exit.");
-			System.out.print("What would you like to send to the server? ");
-			while ((userInput = stdIn.readLine()) != null) {
-				// We send the input before checking if it was the exit message to give the server an opportunity to close its connection cleanly
-				socketWriter.println(userInput);
-
-				// Check if the user wants to exit
-				if (userInput.equals("exit")) {
-					System.out.println("Exiting.");
-					break;
-				}
-
-				System.out.println("response: " + socketReader.readLine());
-				System.out.print("What would you like to send to the server? ");
-			}
-
-			// Close the socket
-			socket.close();
+			backupClient = new BackupClient();
+			backupClient.start();
 		}
 		catch (Exception e) {
 			System.out.println("Something went wrong: " + e.getMessage());
 		}
+		finally {
+			try {
+				backupClient.cleanUp();
+			}
+			catch (IOException e) {}
+		}
+	}
+
+	private ConfigReader _config;
+	private Socket _socket;
+	private PrintWriter _socketWriter;
+	private BufferedReader _socketReader;
+
+	/**
+	 * Loads the config file and initialises the socket and it's reader/writer helpers.
+	 */
+	public BackupClient() throws Exception {
+		// Load the config file
+		try {
+			_config = new ConfigReader("./clientConfig");
+		}
+		catch (IOException e) {
+			throw new Exception("Could not open config file: " + e.getMessage());
+		}
+
+		String serverIP = this._config.getSetting("serverIP");
+
+		// Get the port number to connect to the server on
+		String portNumberAsString = this._config.getSetting("port");
+		int portNumber;
+		try {
+			portNumber = Integer.parseInt(portNumberAsString);
+		}
+		catch (NumberFormatException e) {
+			throw new Exception("The port number in the config file was not a number.");
+		}
+
+		// Create a socket and the objects for communication
+		this._socket = new Socket(serverIP, portNumber);
+		this._socketWriter = new PrintWriter(this._socket.getOutputStream(), true);
+		this._socketReader = new BufferedReader(new InputStreamReader(this._socket.getInputStream()));
+	}
+
+	/**
+	 * Start the communication with the server.
+	 */
+	public void start() throws Exception {
+		// Send the client's identity to the server
+		String identity = this._config.getSetting("identity");
+		this._socketWriter.println(identity);
+
+		// Check that the server recognised our identity
+		String serverResponse = this._socketReader.readLine();
+		if (!serverResponse.equals("Recognised")) {
+			throw new Exception("The server didn't recognise our identity: " + serverResponse);
+		}
+
+		// Send user's message to the server and print the response
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String userInput;
+
+		System.out.println("If you would like to exit at any time, simply type exit.");
+		System.out.print("What would you like to send to the server? ");
+		while ((userInput = stdIn.readLine()) != null) {
+			// We send the input before checking if it was the exit message to give the server an opportunity to close its connection cleanly
+			this._socketWriter.println(userInput);
+
+			// Check if the user wants to exit
+			if (userInput.equals("exit")) {
+				System.out.println("Exiting.");
+				break;
+			}
+
+			System.out.println("response: " + this._socketReader.readLine());
+			System.out.print("What would you like to send to the server? ");
+		}
+	}
+
+	/**
+	 * Cleans up any buffers, sockets, etc. that need to be closed.
+	 */
+	public void cleanUp() throws IOException {
+		this._socket.close();
+		this._socketWriter.close();
+		this._socketReader.close();
 	}
 }
