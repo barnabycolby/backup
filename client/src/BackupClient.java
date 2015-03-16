4379cf13e5eeee969d749a18db376dc0bfbf3b4c
Barnaby Colby <barneycolby@gmail.com>
2015-03-16 00:32:14 +0000

    Added basic communication between client and server.

--- client/src/BackupClient.java
+++ client/src/BackupClient.java
@@ -1,11 +1,27 @@
 import java.net.*;
+import java.io.*;
 
 public class BackupClient {
 	public static void main(String[] args) {
 		try {
 			String serverHostname = "127.0.0.1";
 
+			// Create a socket and the objects for communication
 			Socket socket = new Socket(serverHostname, 10008);
+			PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// Send user's message to the server and print the response
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			String userInput;
+
+			while ((userInput = stdIn.readLine()) != null) {
+				socketWriter.println(userInput);
+
+				System.out.println("response: " + socketReader.readLine());
+			}
+
+			// Close the socket
 			socket.close();
 		}
 		catch (Exception e) {
--- server/src/ClientHandler.java
+++ server/src/ClientHandler.java
@@ -13,6 +13,21 @@ public class ClientHandler extends Thread {
 	public void run() {
 		try {
 			System.out.println("New client handler started.");
+
+			// Read the message from the client and send a response
+			PrintWriter socketWriter = new PrintWriter(_clientSocket.getOutputStream(), true);
+			BufferedReader socketReader = new BufferedReader(new InputStreamReader(_clientSocket.getInputStream()));
+
+			String inputLine;
+
+			while ((inputLine = socketReader.readLine()) != null) {
+				System.out.println("Message from client: " + inputLine);
+				socketWriter.println("Huh?");
+			}
+
+			// Clean up
+			socketWriter.close();
+			socketReader.close();
 			_clientSocket.close();
 		}
 		catch (IOException e) {
