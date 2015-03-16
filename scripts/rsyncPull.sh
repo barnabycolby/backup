e257e7cfd14acf358f4f4bd65fef3900c0e5b664
Barnaby Colby <barneycolby@gmail.com>
2015-03-16 18:41:30 +0000

    Refactored script to try everything possible to prevent rsync failing. Attempts to mount the share if it has not been mounted.

--- scripts/rsyncPull.sh
+++ scripts/rsyncPull.sh
@@ -21,37 +21,51 @@ shareToPullFrom="${backupShares}/${shareToBackup}"
 # The destination directory to pull from
 destination="${continuousDirectoryPath}/${shareToBackup}"
 
+# Make sure that the share is actually mounted and attempt to mount it if not
 if mount | grep ${shareToPullFrom} > /dev/null
 then
-	# Check that the destination is not a file
-	if [ -f ${destination} ]
+else
+	# Attempt to mount the share
+	# This requires an entry in fstab specifying the options required to mount the share (such as username, password, workgroup)
+	# The entry must have the user option, so that we do not need root priviledges to mount the share
+	echo "Attempting to mount the share"
+	mountOutput="$( mount ${shareToPullFrom} )"
+
+	if [ $? -ne 0 ]
 	then
-		errorMessage="Destination ${destination} is not a directory."
+		errorMessage="Share to pull from could not be mounted: ${mountOutput}"
 		echo ${errorMessage}
 		echo ${errorMessage} | mail -s "${emailSubject}" ${emailAddress}
 		exit 1
 	fi
-	
-	# Check that the destination directory actually exists
-	if [ ! -e ${destination} ]
-	then
-		mkdir ${destination}
-	fi
+fi
 
-	echo "Pulling from the share into the continuous backup directory."
-	# The slash on the end of the source directory is important
-	# Otherwise rsync would create a copy of the folder under the destination tree, adding another unnecessary level of depth
-	rsyncOutput="$( rsync -a --delete ${shareToPullFrom}/ ${destination} 2>&1 )"
-
-	# Check that the rsync operation was successful
-	if [ $? -ne 0 ]
-	then
-		echo "${rsyncOutput}"
-		echo "${rsyncOutput}" | mail -s "${emailSubject}" ${emailAddress}
-	fi
-
-else
-	errorMessage="Share to pull from has not been mounted: ${shareToPullFrom}"
+# Check that the destination is not a file
+if [ -f ${destination} ]
+then
+	errorMessage="Destination ${destination} is not a directory."
 	echo ${errorMessage}
 	echo ${errorMessage} | mail -s "${emailSubject}" ${emailAddress}
+	exit 1
 fi
+
+# Check that the destination directory actually exists
+if [ ! -e ${destination} ]
+then
+	mkdir ${destination}
+fi
+
+echo "Pulling from the share into the continuous backup directory."
+# The slash on the end of the source directory is important
+# Otherwise rsync would create a copy of the folder under the destination tree, adding another unnecessary level of depth
+rsyncOutput="$( rsync -a --delete ${shareToPullFrom}/ ${destination} 2>&1 )"
+
+# Check that the rsync operation was successful
+if [ $? -ne 0 ]
+then
+	echo "${rsyncOutput}"
+	echo "${rsyncOutput}" | mail -s "${emailSubject}" ${emailAddress}
+	exit 1
+fi
+
+echo "Pull was successful"
